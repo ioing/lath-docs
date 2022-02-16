8fdfd6d956ba5ed5a7d19b8e00c6d2a3264cfb7a
lien <ioingnet@gmail.com>
2022-02-16 19:51:36 +0800

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const ip = require('ip')
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const mode = 'development'
+const entrys = {
+  main: './src/index.ts'
+}
+
+module.exports = {
+  mode: mode,
+  devtool: 'source-map',
+  entry: entrys,
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].js'
+  },
+  resolve: {
+    extensions: [".js", ".ts", ".html"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: ["ts-loader"]
+      },
+      {
+        test: /\.html$/i,
+        use: 'text-loader'
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Lath'
+    }),
+  ],
+  devServer:{
+    host: ip.address(),
+    hot: true,
+    open: true
+  },
+  watchOptions: {
+    poll: 500,
+    aggregateTimeout: 100,
+    ignored: '/node_moduels/'
+  }
+}
