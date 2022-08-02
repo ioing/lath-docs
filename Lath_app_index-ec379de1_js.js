ca5dfe384ece6a6bd451ba45528809121a834806
lien <ioingnet@gmail.com>
2022-08-02 19:53:38 +0800

    Add files via upload

---
+++ Lath_app_index-ec379de1_js.js
@@ -0,0 +1,106 @@
+"use strict";
+(self["webpackChunklath_docs"] = self["webpackChunklath_docs"] || []).push([["Lath_app_index-ec379de1_js"],{
+
+/***/ "../Lath/app/index-ec379de1.js":
+/*!*************************************!*\
+  !*** ../Lath/app/index-ec379de1.js ***!
+  \*************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "AppSwitcher": () => (/* binding */ AppSwitcher)
+/* harmony export */ });
+/* harmony import */ var _index_8db447f3_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./index-8db447f3.js */ "../Lath/app/index-8db447f3.js");
+/* harmony import */ var _index_f7caff1c_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./index-f7caff1c.js */ "../Lath/app/index-f7caff1c.js");
+class AppSwitcher{constructor(e){this.inProgress=!1;var{relativeViewport:t,absoluteViewport:i,fixedViewport:s}=e.segue;this.relativeViewport=t,this.absoluteViewport=i,this.fixedViewport=s,this.application=e,this.delayDynamicImport()}delayDynamicImport(){Promise.all(/*! import() */[__webpack_require__.e("vendors-Lath_node_modules_html2canvas_dist_html2canvas_js"), __webpack_require__.e("Lath_app_index-6fb73c44_js")]).then(__webpack_require__.bind(__webpack_require__, /*! ./index-6fb73c44.js */ "../Lath/app/index-6fb73c44.js")).catch}getActiveApplet(){var e=this.application.activityApplet;return null!=e&&e.slide?this.application.applets[e.slide.activeId]:e}blurBackgroundImage(){this.relativeViewport.style.filter="blur(20px)",this.absoluteViewport.style.filter="blur(20px)"}focusBackgroundImage(){this.relativeViewport.style.filter="none",this.absoluteViewport.style.filter="none"}open(){return (0,_index_8db447f3_js__WEBPACK_IMPORTED_MODULE_0__._)(this,void 0,void 0,function*(){!0!==this.inProgress&&(this.inProgress=!0,yield this.createAppSwitcher(),this.blurBackgroundImage(),this.switcher.style.opacity="1",this.inProgress=!1)})}close(){this.inProgress=!0,this.focusBackgroundImage(),this.switcher.style.opacity="0",(0,_index_f7caff1c_js__WEBPACK_IMPORTED_MODULE_1__.s)(()=>{this.fixedViewport.removeChild(this.switcher)},400),this.inProgress=!1}createAppSwitcher(){return (0,_index_8db447f3_js__WEBPACK_IMPORTED_MODULE_0__._)(this,void 0,void 0,function*(){this.switcher=document.createElement("applet-switcher"),this.switcher.style.cssText=`
+      display: grid;
+      box-sizing: border-box;
+      position: absolute;
+      inset: 0;
+      z-index: 2;
+      width: 100%;
+      height: 100%;
+      padding: 10px;
+      padding-top: constant(safe-area-inset-top);
+      padding-top: env(safe-area-inset-top);
+      overflow-y: scroll !important;
+      scroll-padding: 12px;
+      scroll-behavior: smooth;
+      scroll-snap-type: y mandatory;
+      grid-template-columns: 50% 50%;
+      grid-auto-rows: max-content;
+      background: rgb(128 128 128 / 50%);
+      opacity: 0;
+      transition: opacity .6s ease;
+    `;var e=this.relativeViewport.offsetWidth,t=e/2-30,i=this.relativeViewport.offsetHeight*t/e,s=this.application.applets,o=this.getActiveApplet();for(const a in s){const n=s[a],p=n.color;if("applet"===n.rel&&!n.isModality&&n.view){const l=document.createElement("applet-switcher-item"),d=document.createElement("div"),c=document.createElement("div"),h=document.createElement("div"),x=document.createElement("div"),f=document.createElement("div");l.style.cssText=`
+          width: 100%;
+          height: ${50+i}px;
+          padding: 0 10px;
+          box-sizing: border-box;
+          scroll-snap-align: start;
+          cursor: pointer;
+        `;var r=n.config.poster?`
+          background-image: url(${n.config.poster});
+          background-size: cover;
+        `:"";d.style.cssText=`
+          display: block;
+          margin: 20px 0 4px;
+          width: ${t}px;
+          height: ${i}px;
+          background: ${p};
+          border-radius: 16px;
+          overflow: hidden;
+          box-shadow: 0 3px 7px rgb(0 0 0 / 10%);
+          ${r}
+        `,c.style.cssText=`
+          display: block;
+          width: ${t}px;
+          height: ${i}px;
+          background: ${p};
+          border-radius: 16px;
+          overflow: hidden;
+          box-shadow: 0 3px 7px rgb(0 0 0 / 10%);
+        `,x.style.cssText=`
+          display: flex;
+          margin: 0 auto;
+          width: ${t}px;
+          align-items: center;
+        `,h.style.cssText=n.config.icon?`
+          width: 16px;
+          height: 16px;
+          background-image: url(${n.config.icon});
+        `:"display: none;",f.style.cssText=`
+          font-size: 16px;
+          color: #fff;
+          overflow: hidden;
+          white-space: nowrap;
+          text-overflow: ellipsis;
+          font-family: "SF Pro Text","Myriad Set Pro","SF Pro Icons","Apple Legacy Chevron","Helvetica Neue","Helvetica","Arial",sans-serif;
+        `,f.innerText=n.config.title||n.id,x.appendChild(h),x.appendChild(f),d.appendChild(c),l.appendChild(d),l.appendChild(x),this.switcher.appendChild(l),this.fixedViewport.appendChild(this.switcher);const u=c.offsetTop,g=c.offsetLeft,m=c.offsetWidth/this.switcher.offsetWidth;if(n.id===(null==o?void 0:o.id)){const w=c.style.cssText;c.style.cssText=`
+            position: fixed;
+            top: 0;
+            right: 0;
+            left: 0;
+            transform-origin: top left;
+            transform: translate3d(0px, 0px, 0px);
+            transition: all .4s ease;
+            background: ${p};
+            border-radius: ${16/m}px;
+            overflow: hidden;
+          `,(0,_index_f7caff1c_js__WEBPACK_IMPORTED_MODULE_1__.s)(()=>{c.style.transform=`translate3d(${g}px, ${u}px, 0px) scale(${m})`,(0,_index_f7caff1c_js__WEBPACK_IMPORTED_MODULE_1__.s)(()=>{c.style.cssText=w},600)},200),c.appendChild(yield n.captureShot())}else n.captureShot().then(e=>c.appendChild(e));l.addEventListener("click",()=>{c.style.cssText=`
+            position: fixed;
+            top: 0;
+            right: 0;
+            left: 0;
+            transform-origin: top left;
+            transform: translate3d(${g}px, ${u}px, 0px) scale(${m});
+            background: ${p};
+            border-radius: ${16/m}px;
+            overflow: hidden;
+          `,(0,_index_f7caff1c_js__WEBPACK_IMPORTED_MODULE_1__.r)(()=>{const i=c.querySelector("canvas");c.style.transition="all .4s ease",i.style.transition="all .4s ease",(0,_index_f7caff1c_js__WEBPACK_IMPORTED_MODULE_1__.r)(()=>{c.style.transform="translate3d(0px, 0px, 0px) scale(1)";const t=Date.now();this.application.to(n.id,n.param).then(()=>{var e=Date.now()-t;(0,_index_f7caff1c_js__WEBPACK_IMPORTED_MODULE_1__.s)(()=>{c.style.borderRadius="0px",i.style.borderRadius="0px",this.close()},200<e?0:400-e)})})})})}}})}}
+
+/***/ })
+
+}]);
+//# sourceMappingURL=Lath_app_index-ec379de1_js.js.map
---
+++ Lath_app_slide-native-2a307629_js.js
@@ -0,0 +1,20 @@
+"use strict";
+(self["webpackChunklath_docs"] = self["webpackChunklath_docs"] || []).push([["Lath_app_slide-native-2a307629_js"],{
+
+/***/ "../Lath/app/slide-native-2a307629.js":
+/*!********************************************!*\
+  !*** ../Lath/app/slide-native-2a307629.js ***!
+  \********************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (/* binding */ slideNative)
+/* harmony export */ });
+/* harmony import */ var _index_8db447f3_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./index-8db447f3.js */ "../Lath/app/index-8db447f3.js");
+var slideNative=o=>(0,_index_8db447f3_js__WEBPACK_IMPORTED_MODULE_0__._)(void 0,void 0,void 0,function*(){var e,i=o.swipeTransitionType;return o.reverse?!0===(null==(e=o.applets[1].controls)?void 0:e.visibility)&&("slide"===i?(yield o.in.duration(0).to("-30%",0,0).end(),o.in.duration(400).ease("ease-out").to(0,0,0).end()):(yield o.in.duration(0).to(0,0,0).scale(.97).end(),o.in.duration(400).to(0,0,0).scale(1).end()),yield null==(e=o.applets[1].controls)?void 0:e.hide()):(yield o.in.duration(0).to(0,0,0).end(),("slide"===i?o.out.duration(400).ease("ease-out").to("-30%",0,0):o.out.duration(400).to(0,0,0).scale(.97)).end(),yield null==(e=o.applets[0].controls)?void 0:e.show()),Promise.resolve(!1)});
+
+/***/ })
+
+}]);
+//# sourceMappingURL=Lath_app_slide-native-2a307629_js.js.map
