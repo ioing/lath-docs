3f8516d5f2baa2958a7b2b1d8471967e2718d1d1
lien <ioingnet@gmail.com>
2022-08-22 20:27:07 +0800

    Add files via upload

---
+++ Lath_app_slide-native-53a4870e_js.js
@@ -0,0 +1,20 @@
+"use strict";
+(self["webpackChunklath_docs"] = self["webpackChunklath_docs"] || []).push([["Lath_app_slide-native-53a4870e_js"],{
+
+/***/ "../Lath/app/slide-native-53a4870e.js":
+/*!********************************************!*\
+  !*** ../Lath/app/slide-native-53a4870e.js ***!
+  \********************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (/* binding */ slideNative)
+/* harmony export */ });
+/* harmony import */ var _index_6f074738_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./index-6f074738.js */ "../Lath/app/index-6f074738.js");
+var slideNative=o=>(0,_index_6f074738_js__WEBPACK_IMPORTED_MODULE_0__._)(void 0,void 0,void 0,function*(){var e,i=o.swipeTransitionType;return o.reverse?!0===(null==(e=o.applets[1].controls)?void 0:e.visibility)&&(("slide"===i?(yield o.in.duration(0).to("-30%",0,0).end(),o.in.duration(400).ease("ease-out").to(0,0,0)):(yield o.in.duration(0).to(0,0,0).scale(.97).end(),o.in.duration(400).to(0,0,0).scale(1))).end(),yield null==(e=o.applets[1].controls)?void 0:e.hide()):(yield o.in.duration(0).to(0,0,0).end(),("slide"===i?o.out.duration(400).ease("ease-out").to("-30%",0,0):o.out.duration(400).to(0,0,0).scale(.97)).end(),yield null==(e=o.applets[0].controls)?void 0:e.show()),Promise.resolve(!1)});
+
+/***/ })
+
+}]);
+//# sourceMappingURL=Lath_app_slide-native-53a4870e_js.js.map
