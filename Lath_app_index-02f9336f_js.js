2bc40cea5af669a7c548fe233ecaac0f8be07f15
lien <ioingnet@gmail.com>
2022-09-01 15:18:43 +0800

    Add files via upload

---
+++ Lath_app_index-02f9336f_js.js
@@ -0,0 +1,27 @@
+"use strict";
+(self["webpackChunklath_docs"] = self["webpackChunklath_docs"] || []).push([["Lath_app_index-02f9336f_js"],{
+
+/***/ "../Lath/app/index-02f9336f.js":
+/*!*************************************!*\
+  !*** ../Lath/app/index-02f9336f.js ***!
+  \*************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "capture": () => (/* binding */ capture)
+/* harmony export */ });
+/* harmony import */ var _index_08f13669_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./index-08f13669.js */ "../Lath/app/index-08f13669.js");
+/* harmony import */ var html2canvas__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! html2canvas */ "../Lath/node_modules/html2canvas/dist/html2canvas.js");
+/* harmony import */ var html2canvas__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(html2canvas__WEBPACK_IMPORTED_MODULE_1__);
+const capture=n=>(0,_index_08f13669_js__WEBPACK_IMPORTED_MODULE_0__._)(void 0,void 0,void 0,function*(){var t=n.contentWindow&&n.sameOrigin?n.contentWindow.document.documentElement:n.view||document.createElement("div"),e=n.viewport;if(!e)throw"Capture Shot: The applet has not been initialized!";var o=e.offsetWidth,e=e.offsetHeight;const i=yield html2canvas__WEBPACK_IMPORTED_MODULE_1___default()(t,{backgroundColor:n.color,useCORS:!0,width:o,height:e,windowWidth:o,windowHeight:e,x:t.scrollLeft,y:t.scrollTop,imageTimeout:2e3});return i.style.cssText=`
+    width: 100% !important;
+    height: auto !important;
+    transition: all .4s ease;
+    border-radius: 16px;
+  `,i});
+
+/***/ })
+
+}]);
+//# sourceMappingURL=Lath_app_index-02f9336f_js.js.map
