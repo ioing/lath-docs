26a45f9793cd1cdf1a268456c1663d89328046e7
lien <ioingnet@gmail.com>
2022-09-23 20:34:21 +0800

    Add files via upload

---
+++ Lath_app_auto_cjs-5c90c95e_js.js
@@ -0,0 +1,14 @@
+(self["webpackChunklath_docs"] = self["webpackChunklath_docs"] || []).push([["Lath_app_auto_cjs-5c90c95e_js"],{
+
+/***/ "../Lath/app/auto.cjs-5c90c95e.js":
+/*!****************************************!*\
+  !*** ../Lath/app/auto.cjs-5c90c95e.js ***!
+  \****************************************/
+/***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
+
+function _interopDefaultLegacy(l){return l&&"object"==typeof l&&"default"in l?l:{default:l}}var install=__webpack_require__(/*! scroll-polyfill */ "../Lath/node_modules/scroll-polyfill/dist/index.esm.js"),install__default=_interopDefaultLegacy(install);install__default.default();
+
+/***/ })
+
+}]);
+//# sourceMappingURL=Lath_app_auto_cjs-5c90c95e_js.js.map
