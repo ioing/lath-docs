975dc7380c93a98512a4a40f340bf7005d1480f0
lien <ioingnet@gmail.com>
2022-09-23 20:36:31 +0800

    Add files via upload

---
+++ sw1.js
@@ -0,0 +1,34 @@
+self.importScripts('cache.js');
+
+// Files to cache
+const cacheName = 'Lath-v8';
+const contentToCache = [
+  '/index.html',
+  '/main.js',
+  ...caches
+];
+
+// Installing Service Worker
+self.addEventListener('install', (e) => {
+  console.log('[Service Worker] Install');
+  e.waitUntil((async () => {
+    const cache = await caches.open(cacheName);
+    console.log('[Service Worker] Caching all: app shell and content');
+    if (!cache) return;
+    await cache.addAll(contentToCache);
+  })());
+});
+
+// Fetching content using Service Worker
+self.addEventListener('fetch', (e) => {
+  e.respondWith((async () => {
+    const r = await caches.match(e.request);
+    console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+    if (r) return r;
+    const response = await fetch(e.request);
+    const cache = await caches.open(cacheName);
+    console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+    cache.put(e.request, response.clone());
+    return response;
+  })());
+});
