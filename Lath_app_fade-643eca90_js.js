8774d4e7722ff4f7f35280adfbc5526eddc100e7
lien <ioingnet@gmail.com>
2022-09-23 20:35:19 +0800

    Add files via upload

---
+++ Lath_app_fade-643eca90_js.js
@@ -0,0 +1,19 @@
+"use strict";
+(self["webpackChunklath_docs"] = self["webpackChunklath_docs"] || []).push([["Lath_app_fade-643eca90_js"],{
+
+/***/ "../Lath/app/fade-643eca90.js":
+/*!************************************!*\
+  !*** ../Lath/app/fade-643eca90.js ***!
+  \************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (/* binding */ fade)
+/* harmony export */ });
+var fade=i=>a=>{let e,t,o,n;0===i?(e=1,t=0,o=a.in,n=a.out):(e=0,t=1,o=n=a.in),o.duration(0).ease("ease-out-expo").to(0,0,0).opacity(e).end(function(){n.duration(767).opacity(t).end(function(){a.callback(!1)})})};
+
+/***/ })
+
+}]);
+//# sourceMappingURL=Lath_app_fade-643eca90_js.js.map
