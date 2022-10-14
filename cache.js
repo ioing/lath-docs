faa9236eb39ab1dfc1062b615736fc43e07b96f0
lien <ioingnet@gmail.com>
2022-10-14 18:10:49 +0800

    Add files via upload

--- cache.js
+++ cache.js
@@ -1,5 +1,5 @@
 
     var cacheFiles = [
-      "110.js","111.js","132.js","172.js","271.js","271.js.LICENSE.txt","417.js","446.js","48.js","572.js","599.js","60.js","730.js","730.js.LICENSE.txt","77.js","867.js","867.js.LICENSE.txt","899.js","925.js","947.js","969.js","index.html","main.js","main.js.LICENSE.txt","main.js.map"
+      "111.js","132.js","172.js","208.js","248.js","271.js","271.js.LICENSE.txt","301.js","375.js","417.js","446.js","682.js","730.js","730.js.LICENSE.txt","794.js","817.js","867.js","867.js.LICENSE.txt","947.js","967.js","969.js","index.html","main.js","main.js.LICENSE.txt","main.js.map"
     ]
   
--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 self.importScripts('cache.js');
 
 // Files to cache
-const cacheName = 'Lath-v19';
+const cacheName = 'Lath-v20';
 const contentToCache = [
   ...cacheFiles
 ];
