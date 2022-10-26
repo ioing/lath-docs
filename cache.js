913a3ba8384323835a41bc8c27131a34b5a7dfa9
lien <ioingnet@gmail.com>
2022-10-26 22:35:21 +0800

    Add files via upload

--- cache.js
+++ cache.js
@@ -1,5 +1,5 @@
 
     var cacheFiles = [
-      "172.js","245.js","280.js","334.js","354.js","39.js","439.js","446.js","447.js","54.js","542.js","568.js","591.js","591.js.LICENSE.txt","730.js","730.js.LICENSE.txt","779.js","867.js","867.js.LICENSE.txt","969.js","996.js","index.html","main.js","main.js.LICENSE.txt","main.js.map"
+      "106.js","172.js","195.js","280.js","393.js","446.js","489.js","550.js","550.js.LICENSE.txt","6.js","61.js","673.js","698.js","730.js","730.js.LICENSE.txt","804.js","804.js.LICENSE.txt","828.js","852.js","867.js","867.js.LICENSE.txt","969.js","index.html","main.js","main.js.LICENSE.txt","main.js.map"
     ]
   
