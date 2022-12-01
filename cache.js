9df1cada8d3fa2f10d1b789e640a2c80ecab9fa0
lien <ioingnet@gmail.com>
2022-12-01 17:45:53 +0800

    Add files via upload

--- cache.js
+++ cache.js
@@ -1,5 +1,5 @@
 
     var cacheFiles = [
-      "13.js","13.js.LICENSE.txt","186.js","186.js.LICENSE.txt","242.js","281.js","281.js.LICENSE.txt","469.js","469.js.LICENSE.txt","470.js","471.js","471.js.LICENSE.txt","540.js","540.js.LICENSE.txt","740.js","740.js.LICENSE.txt","746.js","746.js.LICENSE.txt","749.js","776.js","776.js.LICENSE.txt","811.js","890.js","939.js","939.js.LICENSE.txt","945.js","945.js.LICENSE.txt","967.js","967.js.LICENSE.txt","996.js","996.js.LICENSE.txt","index.html","main.js","main.js.LICENSE.txt","main.js.map"
+      "102.js","220.js","231.js","242.js","280.js","387.js","423.js","44.js","471.js","471.js.LICENSE.txt","521.js","542.js","61.js","690.js","811.js","813.js","813.js.LICENSE.txt","890.js","891.js","939.js","939.js.LICENSE.txt","index.html","main.js","main.js.LICENSE.txt","main.js.map"
     ]
   
--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 self.importScripts('cache.js');
 
 // Files to cache
-const cacheName = 'Lath-1-v50';
+const cacheName = 'Lath-1-v51';
 const contentToCache = [
   ...cacheFiles
 ];
