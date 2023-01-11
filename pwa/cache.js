fb549829430f631e3926ca0602c740a411743dc8
連山 <nanji.ly@alibaba-inc.com>
2023-01-11 16:01:43 +0800

    feat: a

--- pwa/cache.js
+++ pwa/cache.js
@@ -1,5 +1,5 @@
 
     var cacheFiles = [
-      "182.js","246.js","246.js.LICENSE.txt","304.js","365.js","377.js","377.js.LICENSE.txt","424.js","464.js","48.js","70.js","700.js","716.js","777.js","850.js","854.js","883.js","883.js.LICENSE.txt","917.js","985.js","index.html","main.js","main.js.LICENSE.txt","main.js.map"
+      "0.js","182.js","215.js","215.js.LICENSE.txt","246.js","246.js.LICENSE.txt","295.js","358.js","377.js","377.js.LICENSE.txt","442.js","458.js","48.js","521.js","537.js","599.js","772.js","83.js","906.js","985.js","index.html","main.js","main.js.LICENSE.txt","main.js.map"
     ]
   
