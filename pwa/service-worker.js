0438863e70bfa5cce2f495252b9fe3d475c0ea51
連山 <nanji.ly@alibaba-inc.com>
2023-01-11 18:16:30 +0800

    feat: a

--- pwa/service-worker.js
+++ pwa/service-worker.js
@@ -1,7 +1,7 @@
 self.importScripts('cache.js');
 
 // Files to cache
-const cacheName = 'Lath-1-v63';
+const cacheName = 'Lath-1-v64';
 const contentToCache = [
   ...cacheFiles
 ];
--- src/applets/frameworks/manifest.ts
+++ src/applets/frameworks/manifest.ts
@@ -8,7 +8,7 @@ export default {
     title: 'LATH',
     level: 0,
     free: false,
-    prerender: ['home', 'directory', 'empty', 'docs', 'news', 'weChatQrCode', 'weChatPay'],
+    prerender: ['home', 'directory', 'empty', 'docs', 'support', 'weChatQrCode', 'weChatPay'],
     // swipeModel: true, // 默认 android 以及 pc 和 不支持 touch 的设备为关闭
     pullToRefresh: true,
     animation: 'slide',
