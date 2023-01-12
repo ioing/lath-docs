749290a98dbd0f5e9d8ba3e55591d434c9db656c
連山 <nanji.ly@alibaba-inc.com>
2023-01-12 14:21:42 +0800

    feat: 任务管理器增加删除

--- docs/cache.js
+++ docs/cache.js
@@ -1,5 +1,5 @@
 
     var cacheFiles = [
-      "165.js","195.js","246.js","246.js.LICENSE.txt","264.js","377.js","377.js.LICENSE.txt","439.js","48.js","552.js","596.js","647.js","647.js.LICENSE.txt","662.js","700.js","708.js","728.js","735.js","760.js","985.js","index.html","main.js","main.js.LICENSE.txt","main.js.map"
+      "105.js","193.js","208.js","246.js","246.js.LICENSE.txt","295.js","306.js","377.js","377.js.LICENSE.txt","469.js","48.js","586.js","589.js","625.js","657.js","657.js.LICENSE.txt","70.js","8.js","814.js","985.js","index.html","main.js","main.js.LICENSE.txt","main.js.map"
     ]
   
