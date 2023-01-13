3f83797656cbb7f800a7e21c29b416fac60a6b4f
連山 <nanji.ly@alibaba-inc.com>
2023-01-13 15:12:45 +0800

    fix: 解决任务管理器不显示当前激活的子程序卡

--- pwa/cache.js
+++ pwa/cache.js
@@ -1,5 +1,5 @@
 
     var cacheFiles = [
-      "246.js","246.js.LICENSE.txt","333.js","339.js","377.js","377.js.LICENSE.txt","413.js","48.js","545.js","588.js","599.js","649.js","674.js","674.js.LICENSE.txt","696.js","705.js","832.js","937.js","956.js","985.js","index.html","main.js","main.js.LICENSE.txt","main.js.map"
+      "246.js","246.js.LICENSE.txt","287.js","314.js","377.js","377.js.LICENSE.txt","48.js","653.js","707.js","770.js","778.js","824.js","825.js","870.js","884.js","941.js","982.js","985.js","994.js","994.js.LICENSE.txt","index.html","main.js","main.js.LICENSE.txt","main.js.map"
     ]
   
