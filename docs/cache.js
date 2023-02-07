0d2086dfa6df81f549442781cd593d22ad513033
連山 <nanji.ly@alibaba-inc.com>
2023-02-07 18:40:09 +0800

    feat: demo1

--- docs/cache.js
+++ docs/cache.js
@@ -1,5 +1,5 @@
 
     var cacheFiles = [
-      "114.js","175.js","236.js","237.js","246.js","246.js.LICENSE.txt","353.js","377.js","377.js.LICENSE.txt","38.js","48.js","483.js","638.js","730.js","778.js","901.js","945.js","985.js","994.js","994.js.LICENSE.txt","index.html","main.js","main.js.LICENSE.txt","main.js.map"
+      "145.js","172.js","246.js","246.js.LICENSE.txt","352.js","377.js","377.js.LICENSE.txt","454.js","48.js","614.js","648.js","708.js","758.js","768.js","778.js","789.js","83.js","985.js","994.js","994.js.LICENSE.txt","index.html","main.js","main.js.LICENSE.txt","main.js.map"
     ]
   
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,7 +1,7 @@
 self.importScripts('cache.js');
 
 // Files to cache
-const cacheName = 'Lath-1-v80';
+const cacheName = 'Lath-1-v81';
 const contentToCache = [
   ...cacheFiles
 ];
