9d5f846afc3245ba2bbc775281608331841cbdad
連山 <nanji.ly@alibaba-inc.com>
2023-02-16 19:59:41 +0800

    docs: appswitch

--- pwa/cache.js
+++ pwa/cache.js
@@ -1,5 +1,5 @@
 
     var cacheFiles = [
-      "145.js","172.js","246.js","246.js.LICENSE.txt","352.js","377.js","377.js.LICENSE.txt","454.js","48.js","614.js","648.js","708.js","758.js","768.js","778.js","789.js","83.js","985.js","994.js","994.js.LICENSE.txt","index.html","main.js","main.js.LICENSE.txt","main.js.map"
+      "174.js","22.js","246.js","246.js.LICENSE.txt","282.js","320.js","364.js","377.js","377.js.LICENSE.txt","48.js","508.js","548.js","690.js","814.js","847.js","863.js","886.js","886.js.LICENSE.txt","945.js","985.js","index.html","main.js","main.js.LICENSE.txt","main.js.map"
     ]
   
