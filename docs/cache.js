d4c1374cf3502cc5ee5f6c114e6cf3ae40529ed1
連山 <nanji.ly@alibaba-inc.com>
2023-05-10 21:07:59 +0800

    fix: animation clear

--- docs/cache.js
+++ docs/cache.js
@@ -1,5 +1,5 @@
 
     var cacheFiles = [
-      "246.js","246.js.LICENSE.txt","319.js","336.js","377.js","377.js.LICENSE.txt","378.js","391.js","442.js","48.js","606.js","637.js","64.js","64.js.LICENSE.txt","670.js","700.js","827.js","873.js","937.js","960.js","985.js","index.html","main.js","main.js.LICENSE.txt","main.js.map"
+      "201.js","246.js","246.js.LICENSE.txt","372.js","377.js","377.js.LICENSE.txt","391.js","452.js","48.js","496.js","522.js","529.js","572.js","64.js","64.js.LICENSE.txt","687.js","763.js","827.js","848.js","86.js","985.js","index.html","main.js","main.js.LICENSE.txt","main.js.map"
     ]
   
