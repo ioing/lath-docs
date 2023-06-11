1f067062abc5ad9824c61f892fd144ef9248b40b
連山 <nanji.ly@alibaba-inc.com>
2023-06-11 11:32:43 +0800

    fix: AppletSegue

--- docs/cache.js
+++ docs/cache.js
@@ -1,5 +1,5 @@
 
     var cacheFiles = [
-      "201.js","246.js","246.js.LICENSE.txt","372.js","377.js","377.js.LICENSE.txt","391.js","452.js","48.js","496.js","522.js","529.js","572.js","64.js","64.js.LICENSE.txt","687.js","763.js","827.js","848.js","86.js","985.js","index.html","main.js","main.js.LICENSE.txt","main.js.map"
+      "120.js","144.js","151.js","246.js","246.js.LICENSE.txt","377.js","377.js.LICENSE.txt","424.js","426.js","48.js","51.js","642.js","642.js.LICENSE.txt","736.js","765.js","792.js","804.js","937.js","950.js","963.js","985.js","index.html","main.js","main.js.LICENSE.txt","main.js.map"
     ]
   
