0973c6fc00a9e1ed40ac10a177160f91f71551b8
連山 <nanji.ly@alibaba-inc.com>
2023-06-12 10:51:24 +0800

    fix: AppletSegue

--- pwa/cache.js
+++ pwa/cache.js
@@ -1,5 +1,5 @@
 
     var cacheFiles = [
-      "100.js","185.js","225.js","246.js","246.js.LICENSE.txt","297.js","335.js","344.js","377.js","377.js.LICENSE.txt","409.js","48.js","493.js","632.js","825.js","825.js.LICENSE.txt","827.js","828.js","84.js","97.js","985.js","index.html","main.js","main.js.LICENSE.txt","main.js.map"
+      "158.js","177.js","230.js","246.js","246.js.LICENSE.txt","304.js","357.js","368.js","377.js","377.js.LICENSE.txt","39.js","436.js","48.js","63.js","683.js","727.js","733.js","832.js","9.js","9.js.LICENSE.txt","985.js","index.html","main.js","main.js.LICENSE.txt","main.js.map"
     ]
   
